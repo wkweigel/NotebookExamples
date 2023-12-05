4507f5f1e8efe590997bc99c930b60cf5596dc3f
William K. Weigel <109717854+wkweigel@users.noreply.github.com>
2023-12-05 13:24:07 -0700

    Add files via upload

---
+++ VinaExamples/utilities/getbox.py
@@ -0,0 +1,31 @@
+from pymol import cmd
+
+def getbox(selection='sele', extending = 6.0, software='vina'):
+    
+    ([minX, minY, minZ],[maxX, maxY, maxZ]) = cmd.get_extent(selection)
+
+    minX = minX - float(extending)
+    minY = minY - float(extending)
+    minZ = minZ - float(extending)
+    maxX = maxX + float(extending)
+    maxY = maxY + float(extending)
+    maxZ = maxZ + float(extending)
+    
+    SizeX = maxX - minX
+    SizeY = maxY - minY
+    SizeZ = maxZ - minZ
+    CenterX =  (maxX + minX)/2
+    CenterY =  (maxY + minY)/2
+    CenterZ =  (maxZ + minZ)/2
+    
+    cmd.delete('all')
+    
+    if software == 'vina':
+        return {'center_x':CenterX,'center_y': CenterY, 'center_z': CenterZ},{'size_x':SizeX,'size_y': SizeY,'size_z': SizeZ}
+    elif software == 'ledock':
+        return {'minX':minX, 'maxX': maxX},{'minY':minY, 'maxY':maxY}, {'minZ':minZ,'maxZ':maxZ}
+    elif software == 'both':
+        return ({'center_x':CenterX,'center_y': CenterY, 'center_z': CenterZ},{'size_x':SizeX,'size_y': SizeY,'size_z': SizeZ}),({'minX':minX, 'maxX': maxX},{'minY':minY, 'maxY':maxY}, {'minZ':minZ,'maxZ':maxZ})
+    
+    else:
+        print('software options must be "vina", "ledock" or "both"')
